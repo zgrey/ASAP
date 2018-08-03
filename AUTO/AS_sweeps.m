7f484281e33f8fc9ae8c95bc921ddbabef75a48d
Zach <zgrey334@gmail.com>
2018-08-02 18:43:26 -0600

    Updates to AS sweep visualizations.

--- AUTO/AS_sweeps.m
+++ AUTO/AS_sweeps.m
@@ -15,8 +15,8 @@ W1 = W(:,1); W2 = W(:,2:end);
 T = 50; Y = linspace(min(X*W1),max(X*W1),T)'; Y = [Y; Y(end:-1:1)];
 % compute active coordinates (uncomment inactive variables of interest)
 % Z = zeros(2*T,9); Zstr = '_Z0';
-% rng(47); Z = repmat(2*rand(1,9) - 1,2*T,1); Zstr = '_Zrnd1'; %WARNING: This will extrapolate beyond bounds
-rng(48); Z = repmat(2*rand(1,9) - 1,2*T,1); Zstr = '_Zrnd2'; %WARNING: This will extrapolate beyond bounds
+rng(47); Z = repmat(2*rand(1,9) - 1,2*T,1); Zstr = '_Zrnd1'; %WARNING: This will extrapolate beyond bounds
+% rng(48); Z = repmat(2*rand(1,9) - 1,2*T,1); Zstr = '_Zrnd2'; %WARNING: This will extrapolate beyond bounds
 XX = Y*W1' + Z*W2'; XX0 = bsxfun(@plus,lb0,bsxfun(@times,ub0-lb0,0.5*(XX+1)));
 % determine valid designs
 indu = max(( XX(1:T,:) - ones(T,m) > 0 ),[],2); indl = max(( -ones(T,m) - XX(1:T,:) > 0),[],2);
@@ -26,9 +26,11 @@ subplot(2,1,1), scatter(X*W1,F,30,'filled'); hold on;
 % function approximation plot
 subplot(2,1,1), plot(Y1,Fh,'k--','linewidth',2);
 % plot extrapolated points
-subplot(2,1,1), scatter(XX(indu,:)*W1,poly_predict(XX(indu,:)*W1,Coef,p),30,'rx','LineWidth',2); 
-subplot(2,1,1), scatter(XX(indl,:)*W1,poly_predict(XX(indl,:)*W1,Coef,p),30,'rx','LineWidth',2);
-% nominal shape
+subplot(2,1,1), plot([min(XX(indu,:)*W1),max(XX(indu,:)*W1)],...
+                     poly_predict([min(XX(indu,:)*W1); max(XX(indu,:)*W1)],Coef,p),'rx-','LineWidth',2,'MarkerSize',10); 
+subplot(2,1,1), plot([min(XX(indl,:)*W1),max(XX(indl,:)*W1)],...
+                     poly_predict([min(XX(indl,:)*W1); max(XX(indl,:)*W1)],Coef,p),'rx-','LineWidth',2,'MarkerSize',10);
+% compute nominal shape
 M  = diag(2./(ub0-lb0)); b = -(M*lb0' + ones(m,1));
 x0 = -inv(M)*b; [coordU0,coordL0] = cst_airfoil(l',x0(1:m/2),x0(m/2+1:m),0);
 
@@ -37,19 +39,25 @@ filename = ['./subspaces/AS_sweep_m',num2str(m),'_N',num2str(N),'_pm',num2str(pc
 % plot nominal shape
 subplot(2,1,2), plot(l,coordU0(2,:),'--','LineWidth',2,'color',0.5*ones(3,1)); hold on; axis equal;
 ax = subplot(2,1,2); plot(l,coordL0(2,:),'--','LineWidth',2,'color',0.5*ones(3,1));
-for ii=1:2*T
+for i=1:2*T
     % generate airfoil over sweep
-    [coordU,coordL] = cst_airfoil(l',XX0(ii,1:m/2),XX0(ii,m/2+1:m),0);
+    [coordU,coordL] = cst_airfoil(l',XX0(i,1:m/2),XX0(i,m/2+1:m),0);
     % sweep plot
-    subplot(2,1,1), h1 = scatter(Y(ii),poly_predict(Y(ii),Coef,p),50,'filled','r');
+    subplot(2,1,1), h1 = scatter(Y(i),poly_predict(Y(i),Coef,p),50,'filled','r');
     % airfoil plots
-    subplot(2,1,2), h2 = plot(l,coordU(2,:),'b','LineWidth',2); hold on; 
-    subplot(2,1,2), h3 = plot(l,coordL(2,:),'b','LineWidth',2); axis([ax.XLim, ax.YLim]);
-    fig.CurrentAxes.Visible = 'off'; fig.CurrentAxes.DataAspectRatio = [1 1 3];
+    if  max(( XX(i,:) - ones(1,m) > 0 ),[],2) == 0 && max(( -ones(1,m) - XX(i,:) > 0),[],2) == 0
+        subplot(2,1,2), h2 = plot(l,coordU(2,:),'b','LineWidth',2); hold on; 
+        subplot(2,1,2), h3 = plot(l,coordL(2,:),'b','LineWidth',2); axis([ax.XLim, ax.YLim]);
+        fig.CurrentAxes.Visible = 'off'; fig.CurrentAxes.DataAspectRatio = [1 1 3];
+    else
+        subplot(2,1,2), h2 = plot(l,coordU(2,:),'r','LineWidth',2); hold on; 
+        subplot(2,1,2), h3 = plot(l,coordL(2,:),'r','LineWidth',2); axis([ax.XLim, ax.YLim]);
+        fig.CurrentAxes.Visible = 'off'; fig.CurrentAxes.DataAspectRatio = [1 1 3];
+    end
      % build gif
     figure(fig); frame = getframe(fig); 
     [A,map] = rgb2ind(frame2im(frame),256);
-    if ii == 1
+    if i == 1
         imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',0.1);
     else
         imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',0.1);
